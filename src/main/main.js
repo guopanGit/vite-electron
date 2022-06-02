4279bf00770f51261bfabeca5f996206e3a569a6
gp <19919951645@163.com>
2022-06-02 15:19:37 +0800

    系统托盘

--- src/main/main.js
+++ src/main/main.js
@@ -1,24 +1,31 @@
-const {app, BrowserWindow, ipcMain, Menu} = require('electron')
+//热更新
+try {
+  require('electron-reloader')(module)
+} catch (_) {
+}
+
+const {app, BrowserWindow, ipcMain, dialog, Menu, Tray} = require('electron')
 const path = require('path')
 
 let mainWindow;
+let appTray = null;
 
 function createWindow() {
   const url = path.join(__dirname, '../../dist/index.html')
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
-    icon: path.join(__dirname,"./src/render/assets/DWS.ico"),
+    icon: path.join(__dirname, "./public/DWS.ico"),
+    frame: false,//去掉默认菜单
     webPreferences: {
       nodeIntegration: true,
-      enableRemoteModule: true
+      enableRemoteModule: true,
+      contextIsolation: false,
+      webSecurity: false
     }
   })
-  // mainWindow.loadURL('http://localhost:3000/')
-  mainWindow.loadFile(url)
-
-  //去掉默认菜单栏
-  Menu.setApplicationMenu(null);
+  mainWindow.loadURL('http://localhost:3000/')
+  // mainWindow.loadFile(url)
 
   // 调试模式
   mainWindow.webContents.openDevTools()
@@ -42,20 +49,66 @@ if (!gotTheLock) {
   })
 }
 
-app.on('ready', function () {
+//设置托盘图标和菜单
+app.whenReady().then(() => {
+  let trayMenuTemplate = [
+    {
+      label: '打开',
+      click: () => {
+        mainWindow.show();
+      }
+    },
+    {
+      label: '隐藏',
+      click: () => {
+        mainWindow.hide();
+      }
+    },
+    {
+      label: '退出',
+      click: () => {
+        app.quit();
+        app.quit();//因为程序设定关闭为最小化，所以调用两次关闭，防止最大化时一次不能关闭的情况
+      }
+    }
+  ];
+//系统托盘图标
+  let iconPath = path.join(__dirname, "../../public/icon.ico")
+  appTray = new Tray(iconPath);
+  const contextMenu = Menu.buildFromTemplate(trayMenuTemplate);
+
+//设置此托盘图标的悬停提示内容
+  appTray.setToolTip('DWS');
+//设置此图标的上下文菜单
+  appTray.setContextMenu(contextMenu);
+//单击右下角小图标显示应用左键
+  appTray.on('click', () => {
+    mainWindow.show();
+  })
+})
+
+
+app.on('ready', () => {
   createWindow()
 })
 
-app.on('browser-window-blur', (e) => {
-  console.log('blur')
+// 最小化
+ipcMain.on('min', () => {
+  mainWindow.minimize()
 })
 
-app.on('browser-window-focus', (e) => {
-  console.log('focus')
+// 最大化
+ipcMain.on('max', () => {
+  if (mainWindow.isMaximized()) {
+    mainWindow.unmaximize()
+  } else {
+    mainWindow.maximize()
+  }
 })
 
-app.on('before-quit', function (e) {
-  console.log('out');
+// 关闭
+ipcMain.on('close', () => {
+  mainWindow.close()
 })
 
 app.on('window-all-closed', function () {
@@ -67,10 +120,3 @@ app.on('window-all-closed', function () {
 app.on('activate', function () {
   if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
-
-//热更新
-try {
-  require('electron-reloader')(module)
-} catch (_) {
-}
-
